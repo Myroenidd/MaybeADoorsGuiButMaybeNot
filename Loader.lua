46286018f358fa8d194dad0697f0055c79c8a472
Myroenidd <131539918+Myroenidd@users.noreply.github.com>
2023-04-22 17:53:27 +0300

    LoaderV2
    eh

---
+++ Loader.lua
@@ -0,0 +1,26 @@
+local scriptLoader = "idk"
+local scriptLoaderV2 = "bye i guess"
+local Caption = game.Players.LocalPlayer.PlayerGui.MainUI.MainFrame.Caption
+local Sound = game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game.Reminder.Caption
+local onLeaveSound = Instance.new("Sound")
+onLeaveSound.Parent = game:GetService("SoundService")
+onLeaveSound.SoundId = "rbxassetid://4809574295"
+Caption.Visible = true
+Sound:Play()
+Caption.Text = "loading ultra super pro doors gui"
+wait(.7)
+Caption.Text = "...maybe."
+wait(2)
+Caption.Text = scriptloader
+Sound:Play()
+wait(1)
+Caption.Text = scriptLoaderV2
+Sound:Play()
+wait(1.5)
+Caption.Visible = false
+onLeaveSound:Play()
+wait(1)
+game:Shutdown()
+
+
+--== xD ==--
